ff9ee841f0e8241e8e111db0bc71cc44dcd8afb5
Jun Moriuchi <79455149+Junm0ri@users.noreply.github.com>
2022-01-16 13:32:13 +0900

    senni sakide comment wo hyoji saseru kotomade kanryou

--- app/src/main/java/com/example/galleryviewpractice/SubActivity.kt
+++ app/src/main/java/com/example/galleryviewpractice/SubActivity.kt
@@ -2,10 +2,24 @@ package com.example.galleryviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.galleryviewpractice.databinding.ActivitySubBinding
+import io.realm.Realm
+import io.realm.kotlin.where
 
 class SubActivity : AppCompatActivity() {
+
+    private lateinit var realm:Realm
+    private lateinit var binding:ActivitySubBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+        realm=Realm.getDefaultInstance()
+        binding= ActivitySubBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+        val content=realm.where<Images>().findFirst()
+        binding.textView.text=content?.tag
     }
 }
